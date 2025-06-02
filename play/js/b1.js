d83af1f1463402df39ade4d8807b1b41f49fc58b
Turis-Tv <turistv3@gmail.com>
2025-06-02 07:21:37 +0200

    Add files via upload

---
+++ play/js/b1.js
@@ -0,0 +1,4 @@
+﻿      	var url_string = window.location.href;
+var url = new URL(url_string);
+var cid = url.searchParams.get("id"); 
+document.write(" <iframe  id='frame' name='frame' src='https://saraydorf.de/spor/player/webplayer/?s=https://guhjexjm6qlma62smk3l22wiu0d6vn.top/cek.m3u8?u=https://alpha.cf-worker-7df90b083f9d09.workers.dev/live/"+cid+"/playlist.m3u8' width='100%'  height='100%' frameborder='0' allowfullscreen scrolling='no'></iframe>");
